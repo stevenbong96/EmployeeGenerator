fbbaad790f00541ea1648a3cae9fb65319383f44
Steven Bong <stevenseal99@yahoo.com>
2020-10-11 16:57:57 -0700

    Added additional commands to the index.js. Worked on the connection querries to view tables, delete tables, and update tables. Still had error in displaying the tables

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function startPrompt(){
             name:"userChoice",
             message:"What would you like to do?",
             type:"list",
-            choices:["View All Employees", "View All Employees by Department", "View All Employess by Manager", "View All Roles", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Add Role", "Add Department", "Remove Role", "Exit"]
+            choices:["View All Employees", "View All Employees by Department", "View All Employess by Manager", "View All Roles", "View All Departments", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Add Role", "Add Department", "Remove Role", "Remove Department", "Exit"]
         }
     ]).then(function(response){
         console.log(response);
@@ -40,6 +40,8 @@ function startPrompt(){
             viewAllEmployeesByManager();
         } else if (response.userChoice === "View All Roles"){
             viewAllRoles();
+        } else if (response.userChoice === "View All Departments"){
+            viewAllDepartments();
         } else if (response.userChoice === "Add Employee"){
             addEmployee();
         } else if (response.userChoice === "Add Role"){
@@ -54,6 +56,8 @@ function startPrompt(){
             updateEmployeeManager();
         } else if (response.userChoice === "Remove Role"){
             removeRole();
+        } else if (response.userChoice === "Remove Department"){
+            removeDepartment();
         } else if (response.userChoice === "Exit"){
             connection.end();
         } 
@@ -62,34 +66,80 @@ function startPrompt(){
 
 // Set view all employees function
 function viewAllEmployees(){
-    console.log("View All Employees")
+    // console.log("View All Employees")
+    // ================================
+    // connection.query("SELECT first_name, last_name, title, department, salary, manager FROM employee JOIN", function(err){
+    //     if(err) {
+    //         throw err
+    //     }
+    //     console.log("Success!!");
 
-    // Keep asking user prompts
-    startPrompt();
+    //     // Keep asking user prompts
+    //     startPrompt();
+    // })
 }
 
 // Set view all employees by department function
 function viewAllEmployeesByDepartment(){
-    console.log("All Employees By Department");
+    console.log("View All Employees By Department")
+    // connection.query("SELECT first_name, last_name, department FROM ?",
+    // {
 
-    // Keep asking user prompts
-    startPrompt();
+    // }, 
+    // function(err){
+    //     if(err){
+    //         throw err
+    //     }
+    //     console.log("Success!!")
+        
+    //     // Keep asking user prompts
+    //     startPrompt();
+    // })
 }
 
 // Set view all employees by manager function
 function viewAllEmployeesByManager(){
     console.log("All Employees By Manager");
+    // connection.query("SELECT first_name, last_name, manager_id FROM ?",
+    // {
 
-    // Keep asking user prompts
-    startPrompt();
+    // }, function(err){
+    //     if(err){
+    //         throw err
+    //     }
+    //     console.log("Success!!")
+        
+    //     // Keep asking user prompts
+    //     startPrompt();
+    // })
 }
 
 // Set view all roles function
 function viewAllRoles(){
-    console.log("All Roles");
+    // console.log("All Roles");
+    connection.query("SELECT * FROM role", function(err){
+        if(err){
+            throw err
+        }
+        console.log("Success!!")
+        
+        // Keep asking user prompts
+        startPrompt();
+    })
+}
 
-    // Keep asking user prompts
-    startPrompt();
+// Set view all roles function
+function viewAllDepartments(){
+    // console.log("All Roles");
+    connection.query("SELECT * FROM department", function(err){
+        if(err){
+            throw err
+        }
+        console.log("Success!!")
+        
+        // Keep asking user prompts
+        startPrompt();
+    })
 }
 
 // Set add employee function
@@ -193,34 +243,161 @@ function addDepartment(){
 
 }
 
-// Set remove employee function
-function removeEmployee(){
-    console.log("Remove Employee");
-
-    // Keep asking user prompts
-    startPrompt();
-}
-
 // Set update employee role function
 function updateEmployeeRole(){
-    console.log("Update Employee Role");
-
-    // Keep asking user prompts
-    startPrompt();
+    // console.log("Update Employee Role");
+    inquirer.prompt([
+        {
+            name:"updateTitle",
+            message:"What's the new title role?"
+        },
+        {
+            name:"updateSalary",
+            message:"What's the new salary?"
+        },
+        {
+            name:"updateDepartmentId",
+            message:"What's the new department ID?"
+        },
+    ]).then(function(response){
+        // console.log(response);
+        connection.query("UPDATE role SET ? WHERE ?",
+        [
+            {title: response.updateTitle},
+            {salary: response.updateSalary},
+            {department_id: response.updateDepartmentId}
+        ],
+        function(err){
+            if(err){
+                throw err
+            }
+            console.log("Successfully update employee role!")
+            
+            // Keep asking user prompts
+            startPrompt();
+        })
+    })
 }
 
 // Set update employee manager function
 function updateEmployeeManager(){
     console.log("Update Employee Manager");
+    // inquirer.prompt([
+    //     {
+    //         name:"updateFirstName",
+    //         message:"What's the new manager first name?"
+    //     },
+    //     {
+    //         name:"updateLastName",
+    //         message:"What's the new manager last name?"
+    //     },
+    //     {
+    //         name:"updateRoleId",
+    //         message:"What's the new manager role id?"
+    //     },
+    //     {
+    //         name:"updateManagerId",
+    //         message:"What's the new manager ID?"
+    //     },
+    // ]).then(function(response){
+    //     // console.log(response);
+    //     connection.query("UPDATE employee SET ? WHERE ?",
+    //     [
+    //         {first_name: response.updateFirstName},
+    //         {last_name: response.updateLastName},
+    //         {role_id: response.updateRoleId},
+    //         {manager_id: response.updateManagerId}
+    //     ],
+    //     function(err){
+    //         if(err){
+    //             throw err
+    //         }
+    //         console.log("Successfuuly update the employee manager!")
 
-    // Keep asking user prompts
-    startPrompt();
+    //         // Keep asking user prompts
+    //         startPrompt();
+    //     })
+    // })
+}
+
+// Set remove employee function
+function removeEmployee(){
+    console.log("Remove Employee");
+    // inquirer.prompt([
+    //     {
+    //         name:"removeFirstName",
+    //         message:"Please put employee first name"
+    //     },
+    //     {
+    //         name:"removeLastName",
+    //         message:"Please put employee last name"
+    //     },
+    // ]).then(function(response){
+    //     console.log(response);
+    //     connection.query("DELETE FROM employee WHERE ?", 
+    //     {
+    //         first_name: response.removeFirstName,
+    //         last_name: response.removeLastName
+    //     }, 
+    //     function(err){
+    //     if(err){
+    //         throw err
+    //     }
+    //     console.log("Successfully remove an employee from our database!")
+        
+    //     // Keep asking user prompts
+    //     startPrompt();
+    // })
+    // })
 }
 
 // Set remove role function
 function removeRole(){
-    console.log("Remove Role");
+    console.log("Remove Role!");
+    // inquirer.prompt([
+    //     {
+    //         name:"removeTitle",
+    //         message:"What's the role title that you would like to remove?"
+    //     },
+    // ]).then(function(response){
+    //     console.log(response);
+    //     connection.query("DELETE FROM role WHERE ?",
+    //     [
+    //         {title: removeTitle}
+    //     ], function(err){
+    //         if(err){
+    //             throw err
+    //         }
+    //         console.log("Successfully remove the role from our database!")
+            
+    //         // Keep asking user prompts
+    //         startPrompt();
+    //     })
+    // })
+}
 
-    // Keep asking user prompts
-    startPrompt();
+// Set remove department function
+function removeDepartment(){
+    console.log("Remove Department!")
+    // inquirer.prompt([
+    //     {
+    //         name:"removeDepartment",
+    //         message:"What department that you would like to remove?"
+    //     },
+    // ]).then(function(response){
+    //     console.log(response);
+    //     connection.query("DELETE FROM department WHERE ?",
+    //     [
+    //         {name: response.removeDepartment},
+    //     ],
+    //     function(err){
+    //         if(err){
+    //             throw err
+    //         }
+    //         console.log("Successfully removed")
+
+    //         // Keep asking user prompts
+    //         startPrompt();
+    //     })
+    // })
 }
